50eaefbf3e4ea98efe1dcb6ad87055644cb1247a
t.sarker <tanmay.sarker@baruchmail.cuny.edu>
2020-07-15 18:51:42 -0400

    final commit

--- weather.js
+++ weather.js
@@ -1,155 +1,173 @@
+//DEPENDENCIES================================================
 $(document).ready(() => {
-    console.log("ready!");
+  console.log("ready!");
 });
-// DOM ELEMNTS
-// INITAL DATA
-var cityHistory = [];
-// A user types in a city
-$("#city-info-button").on("click", function () {
-    event.preventDefault();
-    var citySearch = $("#citySearch").val();
-    cityHistory.push(citySearch);
-    //window.localStorage.set("history", JSON.stringify(cityHistory));
-    getAndShowWeather(citySearch);
-});
-
-function getAndShowWeather(city) {
-    // Date Display
+//DOM Elements
+//Initial DATA
+//CURRENT WEATHER=======================================================
+var searchBtn = $("#current-weather");
+var citiesArr = [];
+//FUNCTIONS===================================================
+//A users search history is saved
+// function retrieveInfo() {
+//   citiesArr = JSON.parse(localStorage.getItem([“city name:“]));
+//   console.log(citiesArr);
+//   var noDuplicatesArr = [...new Set(citiesArr)];
+//   for (var i = 0; i < noDuplicatesArr.length; i++) {
+//     var pTag = $(“<p>“);
+//     pTag.text(noDuplicatesArr[i]);
+//     $(“.search-history”).append(pTag);
+//   }
+// };
+// retrieveInfo();
+//USER INPUT===================================================
+//A user types a city
+//A user submits their search
+searchBtn.on("click", function (event) {
+  event.preventDefault();
+  //City Search
+  var citySearch = $("#city-search").val();
+  console.log("City Search:", citySearch);
+  var apiKey = "14f6ccf6c898c0ddda97eb93508451eb";
+  var queryURLCurrent =
+    " http://api.openweathermap.org/data/2.5/weather?q=" +
+    citySearch +
+    "&units=imperial&appid=" +
+    apiKey;
+  console.log("queryURL:", queryURLCurrent);
+  //then the currrent weather of that city shows up
+  $.ajax({
+    url: queryURLCurrent,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    // City Name
+    var cityName = response.name;
+    $("#city-name").text(cityName);
+    console.log("cityName:", cityName);
+    // Date
     var date = moment();
     var dateDisplay = date.format("dddd MMMM Do YYYY");
     $("#date").text(dateDisplay);
-    console.log(dateDisplay);
-    // a user submits their search
-    var citySearch = city;
-    var APIKey = "c7629276d88b73d9dee17485c554906b";
-    console.log(citySearch);
-    var queryURL =
-        "http://api.openweathermap.org/data/2.5/weather?q=" +
-        citySearch +
-        "&appid=c7629276d88b73d9dee17485c554906b" +
-        "&units=imperial";
+    console.log("Date Display:", dateDisplay);
+    // Icon
+    var iconCode = response.weather[0].icon;
+    var weatherIcon = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    console.log(iconCode);
+    $("#weather-icon").attr("src", weatherIcon);
+    // Temp
+    var temperature = response.main.temp;
+    $("#temperature").text(temperature);
+    console.log("Temperature:", temperature);
+    // Humidity
+    var humidity = response.main.humidity;
+    $("#humidity").text(humidity);
+    console.log("Humidity", humidity);
+    // Wind Speed
+    var windSpeed = response.wind.speed;
+    $("#wind-speed").text(windSpeed);
+    console.log("Wind Speed:", windSpeed);
+    // UV Index
+    var latitude = response.coord.lat;
+    var longitude = response.coord.lon;
+    var uvURL =
+      "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+      apiKey +
+      "&lat=" +
+      latitude +
+      "&lon=" +
+      longitude;
+    console.log("uvURL:", uvURL);
     $.ajax({
-        url: queryURL,
-        method: "GET",
+      url: uvURL,
+      method: "GET",
     }).then(function (response) {
-        console.log(response);
-        var iconCode = response.weather[0].icon;
-        var iconImage = "http://openweathermap.org/img/w/" + iconCode + ".png";
-        console.log(iconCode);
-        $("#icon-image").attr("src", iconImage);
-        $("#temperature").text(parseInt(response.main.temp));
-        $("#humidity").text(response.main.humidity);
-        $("#wind-speed").text(response.wind.speed);
-        var latitude = response.coord.lat;
-        var longitude = response.coord.lon;
-        var uvURL =
-            "http://api.openweathermap.org/data/2.5/uvi?appid=" +
-            APIKey +
-            "&lat=" +
-            latitude +
-            "&lon=" +
-            longitude;
-        console.log("uvURL:", uvURL);
-        $.ajax({
-            url: uvURL,
-            method: "GET",
-        }).then(function (response) {
-            console.log(response.value);
-            var uvIndex = response.value;
-            $("#uv-index").text(uvIndex);
-            if (uvIndex < 2) {
-                $(".index").attr("class", "low");
-                console.log("You're safe!");
-            }
-            if (uvIndex >= 2 && uvIndex <= 5) {
-                $(".index").attr("class", "moderate");
-                console.log("Getting risky");
-            }
-            if (uvIndex > 5 && uvIndex <= 7) {
-                $(".index").attr("class", "high");
-                console.log("Uh oh!");
-            }
-            if (uvIndex > 7 && uvIndex <= 10) {
-                $(".index").attr("class", "very-high");
-                console.log("You better stay inside!");
-            }
-            if (uvIndex > 10) {
-                $(".index").attr("class", "extreme");
-                console.log("You will ignite on fire");
-            }
-            // five Day Forecast
-            var forecastURL =
-                "http://api.openweathermap.org/data/2.5/forecast?q=" +
-                citySearch +
-                "&appid=c7629276d88b73d9dee17485c554906b" +
-                "&units=imperial";
-            $.ajax({
-                url: forecastURL,
-                method: "GET",
-            }).then(function (response) {
-                console.log(response);
-                // day one
-                var firstDay = moment(response.list[0].dt_txt).format(
-                    "dddd MMMM Do YYYY"
-                );
-                $("#one").text(firstDay);
-                console.log(firstDay);
-                $("#temperature-one").text(parseInt(response.list[3].main.temp));
-                $("#humidity-one").text(response.list[3].main.humidity);
-                $("#wind-speed-one").text(response.list[3].wind.speed);
-                var iconCode = response.list[3].weather[0].icon;
-                var iconImage = "http://openweathermap.org/img/w/" + iconCode + ".png";
-                console.log(iconCode);
-                $("#icon-image1").attr("src", iconImage);
-                // Day Two
-                var secondDay = moment(response.list[11].dt_txt).format(
-                    "dddd MMMM Do YYYY"
-                );
-                $("#two").text(secondDay);
-                $("#temperature-two").text(parseInt(response.list[11].main.temp));
-                $("#humidity-two").text(response.list[11].main.humidity);
-                $("#wind-speed-two").text(response.list[11].wind.speed);
-                var iconCode = response.list[11].weather[0].icon;
-                var iconImage = "http://openweathermap.org/img/w/" + iconCode + ".png";
-                console.log(iconCode);
-                $("#icon-image2").attr("src", iconImage);
-                // Day Three
-                var thirdDay = moment(response.list[19].dt_txt).format(
-                    "dddd MMMM Do YYYY"
-                );
-                $("#three").text(thirdDay);
-                $("#temperature-three").text(parseInt(response.list[19].main.temp));
-                $("#humidity-three").text(response.list[19].main.humidity);
-                $("#wind-speed-three").text(response.list[19].wind.speed);
-                var iconCode = response.list[19].weather[0].icon;
-                var iconImage = "http://openweathermap.org/img/w/" + iconCode + ".png";
-                console.log(iconCode);
-                $("#icon-image3").attr("src", iconImage);
-                // Day Four
-                var fourthDay = moment(response.list[28].dt_txt).format(
-                    "dddd MMMM Do YYYY"
-                );
-                $("#four").text(fourthDay);
-                $("#temperature-four").text(parseInt(response.list[28].main.temp));
-                $("#humidity-four").text(response.list[28].main.humidity);
-                $("#wind-speed-four").text(response.list[28].wind.speed);
-                var iconCode = response.list[28].weather[0].icon;
-                var iconImage = "http://openweathermap.org/img/w/" + iconCode + ".png";
-                console.log(iconCode);
-                $("#icon-image4").attr("src", iconImage);
-                // Day Five
-                var fifthDay = moment(response.list[37].dt_txt).format(
-                    "dddd MMMM Do YYYY"
-                );
-                $("#five").text(fifthDay);
-                $("#temperature-five").text(parseInt(response.list[37].main.temp));
-                $("#humidity-five").text(response.list[37].main.humidity);
-                $("#wind-speed-five").text(response.list[37].wind.speed);
-                var iconCode = response.list[37].weather[0].icon;
-                var iconImage = "http://openweathermap.org/img/w/" + iconCode + ".png";
-                console.log(iconCode);
-                $("#icon-image5").attr("src", iconImage);
-            });
-        });
+      console.log("UV", response);
+      var uvIndex = response.value;
+      $("#uv-index").text(uvIndex);
+      //the color is green for favorable
+      if (uvIndex < 5) {
+        $("#uv-index").attr("class", "low");
+        console.log("You're safe!");
+      }
+      //the color is purple for moderate
+      if (uvIndex > 5 && uvIndex <= 10) {
+        $("#uv-index").attr("class", "moderate");
+        console.log("Uh oh!");
+      }
+      //the color is red for severe
+      if (uvIndex > 10) {
+        $("#uv-index").attr("class", "high");
+        console.log("You will ignite on fire");
+      }
+      console.log("UV Index:", uvIndex);
     });
-}
+  });
+  // 5 Day Forecast
+  //They also see the 5 day forecast of their city that they searched
+  var queryURLForecast =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    citySearch +
+    "&units=imperial&appid=" +
+    apiKey;
+  console.log("queryURLForecast:", queryURLForecast);
+  //then the currrent weather of that city shows up
+  $.ajax({
+    url: queryURLForecast,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    // day one
+    var firstDay = moment(response.list[3].dt_txt).format("dddd MMMM Do YYYY");
+    $("#one").text(firstDay);
+    console.log(firstDay);
+    var iconCode = response.list[3].weather[0].icon;
+    var weatherIcon = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    console.log(iconCode);
+    $("#weather-icon-one").attr("src", weatherIcon);
+    $("#temperature-one").text(response.list[3].main.temp);
+    $("#humidity-one").text(response.list[3].main.humidity);
+    // day two
+    var secondDay = moment(response.list[11].dt_txt).format(
+      "dddd MMMM Do YYYY"
+    );
+    $("#two").text(secondDay);
+    console.log(secondDay);
+    var iconCode = response.list[11].weather[0].icon;
+    var weatherIcon = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    console.log(iconCode);
+    $("#weather-icon-two").attr("src", weatherIcon);
+    $("#temperature-two").text(response.list[11].main.temp);
+    $("#humidity-two").text(response.list[11].main.humidity);
+    // day three
+    var thirdDay = moment(response.list[19].dt_txt).format("dddd MMMM Do YYYY");
+    $("#three").text(thirdDay);
+    console.log(thirdDay);
+    var iconCode = response.list[19].weather[0].icon;
+    var weatherIcon = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    console.log(iconCode);
+    $("#weather-icon-three").attr("src", weatherIcon);
+    $("#temperature-three").text(response.list[19].main.temp);
+    $("#humidity-three").text(response.list[19].main.humidity);
+    // day four
+    var fourthDay = moment(response.list[27].t_txt).format("dddd MMMM Do YYYY");
+    $("#four").text(fourthDay);
+    console.log(fourthDay);
+    var iconCode = response.list[27].weather[0].icon;
+    var weatherIcon = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    console.log(iconCode);
+    $("#weather-icon-four").attr("src", weatherIcon);
+    $("#temperature-four").text(response.list[27].main.temp);
+    $("#humidity-four").text(response.list[27].main.humidity);
+    // day five
+    var fiveDay = moment(response.list[35].dt_txt).format("dddd MMMM Do YYYY");
+    $("#five").text(fiveDay);
+    console.log(fiveDay);
+    var iconCode = response.list[35].weather[0].icon;
+    var weatherIcon = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    console.log(iconCode);
+    $("#weather-icon-five").attr("src", weatherIcon);
+    $("#temperature-five").text(response.list[35].main.temp);
+    $("#humidity-five").text(response.list[35].main.humidity);
+  });
+});
